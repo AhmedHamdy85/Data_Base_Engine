e3f60542a43d9320b8909486b4730e35da1e2277
AhmedHamdy85 <ah2261@fayoum.edu.eg.com>
2025-01-22 13:08:58 +0200

    back up changes

--- export_to_csv.sh
+++ export_to_csv.sh
@@ -1,8 +1,10 @@
 source colors.sh 
+
+back_up=./backup
             read -p "Enter table name to export: " table_name
             table_path="$1/$table_name"
             if [ -f "$table_path" ]; then
-                csv_file="$1/$table_name.csv"
+                csv_file="$back_up/$table_name.csv"
                 echo "$(tail -n1 < $table_path.meta )" > "$csv_file"
                 cat "$table_path" >> "$csv_file"
                 echo -e "${GREEN}Table exported Succesfuly to $csv_file. ${RESET}"
